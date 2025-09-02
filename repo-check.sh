01efaa6abe8d5e9109a698fbadadb106e6ca2b36
Nate Sales <nate@natesales.net>
2025-09-02 18:05:19 -0400

    fix: repo check script

--- repo-check.sh
+++ repo-check.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Fetch NVIDIA package repository data
 nvidia_packages=$(curl -sL https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/Packages.gz | gunzip -)
-
-nvidia_pkgs_in_conf=$(grep -E "^\s*(nvidia-|cuda-)" mkosi.conf | sed 's/^\s*//' | grep -v "^#" | grep "=")
+nvidia_pkgs_in_conf=$(grep -E "^\s*(nvidia-|cuda-)" mkosi.conf | sed 's/^\s*//' | grep -v "^#" | grep "=" | grep -v "\-tinfoil=")
 
 for pkg_line in $nvidia_pkgs_in_conf; do
     pkg_name=$(echo "$pkg_line" | cut -d'=' -f1)
@@ -11,11 +9,15 @@ for pkg_line in $nvidia_pkgs_in_conf; do
     echo "$pkg_name want $expected_version"
     
     available_versions=$(echo "$nvidia_packages" | grep -A 1 "^Package: $pkg_name$" | grep -v "^Package:" | grep -v "^--$" | sed 's/^Version: //')
-    echo "$available_versions" | sed 's/^/    /'
+    for available_version in $available_versions; do
+        if [ "$available_version" = "$expected_version" ]; then
+            echo "    $available_version (+)"
+        else
+            echo "    $available_version"
+        fi
+    done
 
-    if echo "$available_versions" | grep -q "$expected_version"; then
-        echo "    [+] Version found"
-    else
+    if ! echo "$available_versions" | grep -q "$expected_version"; then
         echo "    [!] Version not found"
     fi
 
